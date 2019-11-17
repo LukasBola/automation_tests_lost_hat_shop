b8290648870c865e3b5fd8108617ec6f961d9a6a
LukasBola <lukasz.bola@icloud.com>
2019-11-17 21:30:34 +0100

    adding gitignore

--- scratches/testeneo_try_else_example.py
+++ scratches/testeneo_try_else_example.py
@@ -10,9 +10,9 @@ xpath_list = ['*yolo_this_is_not_xpath*',
 
 def test_list_of_xpath(xpath_list):
     driver = webdriver.Chrome(executable_path = r'E:\projects\webdrivers\chromedriver_win32_78\chromedriver.exe')
+    driver.get('https://antoogle.testoneo.com/')
     for xpath in xpath_list:
         try:
-            driver.get('https://antoogle.testoneo.com/')
             elem = driver.find_element_by_xpath(xpath)
         except selenium.common.exceptions.InvalidSelectorException as error:
             print(f"XPath '{xpath}' is broken!")
