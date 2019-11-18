a0ef1e5bc99caba531ed31d33d4b7657d8d08da1
LukasBola <lukasz.bola@icloud.com>
2019-11-18 15:02:27 +0100

    testeneo_try_else_example update

--- scratches/testeneo_try_else_example.py
+++ scratches/testeneo_try_else_example.py
@@ -1,17 +1,20 @@
 from selenium import webdriver
 import selenium
 
+from settings import TestSettings
 
 xpath_list = ['*yolo_this_is_not_xpath*',
               '//*[@class="this xpath cannot be found"]',
               '//*[@class="h6 mb-3 font-weight-normal"]',
               '//*[@class="btn btn-lg btn-primary btn-block"]',
-              '*test']
+              '*test',
+              '//*[@class="form-control"]']
 
 def test_list_of_xpath(xpath_list):
-    driver = webdriver.Chrome(executable_path = r'E:\projects\webdrivers\chromedriver_win32_78\chromedriver.exe')
+    driver = webdriver.Chrome(TestSettings().executable_path)
     driver.get('https://antoogle.testoneo.com/')
-    for xpath in xpath_list:
+    for lp, xpath in enumerate(xpath_list):
+        print(f"\nTest {lp+1})"+"\n", "="*40)
         try:
             elem = driver.find_element_by_xpath(xpath)
         except selenium.common.exceptions.InvalidSelectorException as error:
